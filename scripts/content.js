eb1a8ec5a035774ae8ff3541f9a41cc746b93f52
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-04-25 13:46:28 +0300

    simplifications

--- scripts/content.js
+++ scripts/content.js
@@ -5,32 +5,30 @@ if (resultsSection) {
     mutations.forEach(mutation => {
       if (mutation.type !== 'childList' || !mutation.addedNodes.length) return;
 
-      const list = Array.from(mutation.addedNodes).find(
-        nodeOfTypes('OL', 'LI')
-      );
-      if (list) appendButtons(list.querySelectorAll(resultLinkSelector));
+      Array.from(mutation.addedNodes)
+        .find(nodeOfTypes('OL', 'LI'))
+        ?.querySelectorAll(resultLinkSelector)
+        .forEach(appendButton);
     });
   }).observe(resultsSection, {childList: true, subtree: true});
 }
 
-function appendButtons(resultsLinks) {
-  resultsLinks.forEach(link => {
-    const grandparentNode = link.parentNode.parentNode;
-    const btnExists = !!grandparentNode.querySelector(siteSearchBtnSelector);
+function appendButton(link) {
+  const grandparentNode = link.parentNode.parentNode;
+  const btnExists = !!grandparentNode.querySelector(siteSearchBtnSelector);
 
-    if (btnExists) return;
+  if (btnExists) return;
 
-    try {
-      var hostname = new URL(link.href).hostname;
-    } catch (error) {
-      return;
-    }
+  try {
+    var hostname = new URL(link.href).hostname;
+  } catch (error) {
+    return;
+  }
 
-    const button = createButton();
-    button.onclick = e => {
-      e.stopPropagation();
-      console.log(hostname);
-    };
-    grandparentNode.append(button);
-  });
+  const button = createButton();
+  button.onclick = e => {
+    e.stopPropagation();
+    console.log(hostname);
+  };
+  grandparentNode.append(button);
 }
