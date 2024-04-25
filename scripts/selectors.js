5f5868c0682960f8811d59756581a1e13aa848d2
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-04-25 15:36:31 +0300

    add selectors for search input and search button

--- scripts/selectors.js
+++ scripts/selectors.js
@@ -1,3 +1,6 @@
+const searchInputID = 'search_form_input';
+const searchButtonID = 'search_button';
+
 const resultsSectionSelector = 'section[data-testid="mainline"]';
 const resultLinkSelector = 'a[data-testid="result-extras-url-link"]';
 const siteSearchBtnSelector = 'button.site-search-btn';
