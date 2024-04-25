c879a689935ec5e818a30c5a72f8ba151340249d
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-04-25 09:52:02 +0300

    simplify selectors

--- scripts/selectors.js
+++ scripts/selectors.js
@@ -1,8 +1,6 @@
-const resultsSectionSelector =
-  '.site-wrapper #web_content_wrapper #react-layout div div div div section[data-testid="mainline"]';
-const resultsListClass = 'react-results--main';
+const resultsSectionSelector = 'section[data-testid="mainline"]';
 const resultLinkSelector = 'a[data-testid="result-extras-url-link"]';
 
 function resultsList(node) {
-  return node.classList.contains(resultsListClass);
+  return node instanceof HTMLOListElement ? node : undefined;
 }
