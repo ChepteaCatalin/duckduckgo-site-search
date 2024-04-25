2e29a6a06b2f6282978c6ced27145071a535ebc4
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-04-25 09:21:11 +0300

    add mutations observer

--- scripts/content.js
+++ scripts/content.js
@@ -1,5 +1,15 @@
-const a = document.getElementById('header_wrapper');
+try {
+  const resultsSection = document.querySelector(resultsSectionSelector);
 
-const b = createButton();
+  if (resultsSection) {
+    observeMutations(resultsSection);
+    const searchResults = document.querySelectorAll(resultLinkSelector);
 
-a.appendChild(b);
+    searchResults.forEach(result => {
+      const button = createButton();
+      result.parentNode.insertBefore(button, result.nextSibling); //FIXME: are you sure?
+    });
+  }
+} catch (error) {
+  console.log('err');
+}
---
+++ scripts/selectors.js
@@ -0,0 +1,4 @@
+const resultsSectionSelector =
+  '.site-wrapper #web_content_wrapper #react-layout div div div div section[data-testid="mainline"]';
+const resultsListSelector = 'ol.react-results--main';
+const resultLinkSelector = 'a[data-testid="result-extras-url-link"]';
