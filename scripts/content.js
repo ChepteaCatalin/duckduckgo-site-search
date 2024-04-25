26f72987b2c9ac588b91cbd0f5c9bec0099e12a0
Cătălin Cheptea <71984989+ChepteaCatalin@users.noreply.github.com>
2024-04-25 09:54:12 +0300

    omit try catch

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,23 @@
-try {
-  const resultsSection = document.querySelector(resultsSectionSelector);
+const resultsSection = document.querySelector(resultsSectionSelector);
 
-  if (resultsSection) {
-    const observer = new MutationObserver(mutations => {
-      mutations.forEach(mutation => {
-        if (mutation.type !== 'childList' || !mutation.addedNodes.length)
-          return;
+if (resultsSection) {
+  const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      if (mutation.type !== 'childList' || !mutation.addedNodes.length) return;
 
-        const list = [...mutation.addedNodes].find(resultsList);
-        if (list) {
-          const searchResults = list.querySelectorAll(resultLinkSelector);
-          appendButton([...searchResults]);
-        }
-      });
+      const list = [...mutation.addedNodes].find(resultsList);
+      if (list) {
+        const resultsLinks = list.querySelectorAll(resultLinkSelector);
+        appendButton([...resultsLinks]);
+      }
     });
+  });
 
-    observer.observe(resultsSection, {childList: true, subtree: true});
-  }
-} catch (error) {
-  console.log('err');
+  observer.observe(resultsSection, {childList: true, subtree: true});
 }
 
-function appendButton(searchResults) {
-  searchResults.forEach(result => {
+function appendButtons(resultsLinks) {
+  resultsLinks.forEach(result => {
     const button = createButton();
     result.parentNode.insertBefore(button, result.nextSibling); //FIXME: are you sure?
   });
